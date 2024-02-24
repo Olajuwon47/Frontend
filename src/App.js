ff4bff1d1fc2caf57f46c8eb995b73618de0cc7b
@olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-02-24 19:09:03 +0100

    Adding more forms to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {} from 'react';
 import './App.css';
-
-
 import LoginSignup from './containers/LoginSignup.js'
+import Table from './Table.js'
 function App() {
-  return (
+  return(
     <div>
+      <Table/>
      <LoginSignup/> 
     </div>
   );
--- src/containers/LoginSignup.js
+++ src/containers/LoginSignup.js
@@ -6,7 +6,7 @@ import user_icon from '../Assest/user.svg';
 const LoginSignup= () => {
   const [action, setAction]= useState('Login');
   return (
-      <div className='container'>
+      <form className='container'>
        <div className='header'>
        <div className='text'>{action}</div>
         <div className='underline'> </div>
@@ -16,22 +16,37 @@ const LoginSignup= () => {
           <img src={user_icon} alt="" />
           <input type='user'placeholder='username' />
        </div>}
+       {action==='Login'?<div></div>: <div className='input'>
+          <input type='surname'placeholder='surname' id='name' name='surname'/>
+      </div>}
+      {action==='Login'?<div></div>: <div className='input'>
+          <input type='first name'placeholder='first name' id='name' name='first name'/>
+      </div>}
+      {action==='Login'?<div></div>: <div className='input'>
+          <input  type='last name'placeholder='last name' id='name' name='last name'/>
+      </div>}
+      {action==='Login'?<div></div>: <div className='input'>
+          <input type="date" id="birthday" name="birthday" />
+      </div>}
           <div className='input'>
           <img src={email_icon} alt=""/>
-          <input type='email'placeholder='email id' />
+          <input type='email'placeholder='@gmail.com'id='email' name='email' />
         </div>
       <div className='input'>
           <img src={password_icon} alt=""/>
-          <input type='password'placeholder='password'/>
+          <input type='password'placeholder='********' id='password' maxlength="12" minLength="6" name='password' required />
       </div>
-        
+      {action==='Login'?<div></div>: <div className='input'>
+      <img src={password_icon} alt=""/>
+          <input type='Repeat password'placeholder='Repeat password' id='password' maxlength="12" minLength="6" name='password' required/>
+      </div>}
        {action==='Sign up'?<div></div>:<div className='forgot-password '>forgot password?<span>click here!</span></div>}
        <div className='submit-container'>
         <div className={action==='Login'?'submit gray':'submit'} onClick={()=>{setAction('Sign up')}}>Sign up</div>
         <div className={action==='Sign up'?'submit gray':'submit'} onClick={()=>{setAction('Login')}}>Login</div>
         </div>
         </div>
-        </div>
+        </form>
   );
 }
 
